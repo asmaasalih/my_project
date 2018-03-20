e5cd87c621e33b47f80fa53ef5758f4a71171e51
Unknown <aemam383@gmail.com>
2018-03-20 14:17:41 +0400

    stores  part2

--- forum/models.py
+++ forum/models.py
@@ -2,7 +2,7 @@ class Member:
 	def __init__(self, name , age):
 		self.name = name
 		self.age=age
-	
+		self.id=0
 
 	def __str__(self):
 		return "name:{} ,age: {},id:{}".format(self.name,self.age,self.id)
--- forum/stores.py
+++ forum/stores.py
@@ -1,11 +1,32 @@
 class MemberStore:
 	members = []
+	last_id= 1
 
 	def add(self,member):
-		self.members.append(member)
+		member.id=MemberStore.last_id
+		MemberStore.members.append(member)
+		MemberStore.last_id += 1
 
 	def get_all(self):
 		return self.members
+
+	def get_by_id(self,id):
+		all_members = self.get_all()
+		a=all_members.index(self.id)  #index of id in the list
+		result=all_members[a]
+		return result
+
+	def entity_exists(self, member):
+		all_members=self.get_all()
+		if self.member in all_members:
+			return True
+		return False
+		
+	def delete(self,id):
+		member_deleted=self.get_by_id(self.id)
+		self.all_members.remove(member_deleted)		
+			
+			
 
 
 
